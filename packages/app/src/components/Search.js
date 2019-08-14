35f2ee3d2d0300aff557d3791da890b13ce76c67
Jonathan Glock <glockjt@gmail.com>
2019-08-14 14:40:38 -0700

    Added debounce.

--- packages/app/src/components/Search.js
+++ packages/app/src/components/Search.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
 /** @jsx jsx */
@@ -18,30 +18,53 @@ const Input = styled.input`
   }
 `;
 
-const Button = styled.button`
-  padding: 16px 32px;
-  font-size: 16px;
-  border: 1px solid #2cb1bc;
-  background-color: #2cb1bc;
-  color: #044e54;
-  font-weight: 600;
-`;
+// Hook
+function useDebounce(value, delay) {
+  // State and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(
+    () => {
+      // Update debounced value after delay
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+
+      // Cancel the timeout if value changes (also on delay change or unmount)
+      // This is how we prevent debounced value from updating if value is changed ...
+      // .. within the delay period. Timeout gets cleared and restarted.
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, delay] // Only re-call effect if value or delay changes
+  );
+
+  return debouncedValue;
+}
 
 const Search = props => {
-  const onClick = async () => {
-    props.onClick(search);
-    window.scrollTo(0, 0);
-  };
-
-  const onKeyDown = e => {
-    if (e.key === "Enter") {
-      onClick();
-    }
-  };
-
   const onChange = e => setSearch(e.target.value);
 
   const [search, setSearch] = useState("");
+
+  const debounceSearchTerm = useDebounce(search, 500);
+
+  const onClick = async term => {
+    props.onClick(term);
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(
+    () => {
+      if (debounceSearchTerm) {
+        onClick(debounceSearchTerm);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [debounceSearchTerm] // Only call effect if debounced search term changes
+  );
+
   return (
     <div
       css={css`
@@ -54,10 +77,8 @@ const Search = props => {
       <Input
         placeholder="Search for repos"
         value={search}
-        onKeyDown={onKeyDown}
         onChange={onChange}
       />
-      <Button onClick={onClick}>Search</Button>
     </div>
   );
 };
